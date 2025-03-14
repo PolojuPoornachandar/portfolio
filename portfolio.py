3db3bba6ad1212625a4de9b3a70d057464b97119
Polo <chandu@Polos-MacBook-Air.local>
2025-03-14 12:33:05 -0700

    Updated portfolio.py with additional text

---
+++ portfolio.py
@@ -0,0 +1,15 @@
+def portfolio():
+    name = "Chandu"
+    skills = ["Python", "Git", "GitHub", "AWS", "Jenkins"]
+    projects = ["Data Pipeline", "CI/CD Pipeline", "GitHub Actions"]
+    experience = ["Student Ambassador", "Front Desk Assistant"]
+
+
+    print(f"👨‍💻 Name: {name}")
+    print("🚀 Skills: " + ", ".join(skills))
+    print("📂 Projects: " + ", ".join(projects))
+    print("💼 Experience: " + ", ".join(experience))
+
+if __name__ == "__main__":
+    portfolio()
+print("This is an updated portfolio!")
